fa5914ee84e912c1da6d5e115badbb9ebbcd46c4
Skulsik <skulsik1@gmail.com>
2023-05-23 16:26:26 +0700

    Решение бага с изображениями. 5

--- catalog/templatetags/FilterExtension.py
+++ catalog/templatetags/FilterExtension.py
@@ -9,13 +9,13 @@ register = template.Library()
 @register.filter
 def mediapath(format_path):
     """ Фильтр. Возвращает модифицированную строку """
-    return mark_safe(f'media/{format_path}')
+    return mark_safe(f'http://127.0.0.1:8000/media/{format_path}')
 
 # Шаблонный тэг
 @register.simple_tag
 def mediapath(format_path):
     """ Тег. Возвращает модифицированную строку """
-    return f'media/{format_path}'
+    return f'http://127.0.0.1:8000/media/{format_path}'
 
 @register.filter
 def string_clipping(format_str: str):
